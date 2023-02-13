35dbed1de4f19659064e12c615705ca5efcbd252
Natalie Kay <nataliekay@northwesternmutual.com>
2023-02-13 11:49:45 -0500

    add mario icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "nes.css/css/nes.min.css";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <i className="nes-mario"></i>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <p>Hello, it&apos;s a me, Mario!</p>
         <a
           className="App-link"
