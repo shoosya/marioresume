8a2b0bf8fef44c39e046c178c89f66f828a2ef92
Natalie Kay <nataliekay@northwesternmutual.com>
2023-02-13 11:52:50 -0500

    add mario icon

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <i className="nes-mario"></i>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <p>Hello, it&apos;s a me, Mario!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
