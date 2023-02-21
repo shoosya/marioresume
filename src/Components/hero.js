a931053306d447fddd8b3930b0846d0d15fd2443
Natalie Kay <nataliekay@northwesternmutual.com>
2023-02-21 13:20:39 -0500

    add hero section

---
+++ src/Components/hero.js
@@ -0,0 +1,10 @@
+// import styled from "styled-components";
+import NesContainer from "./nes-container";
+
+const Hero = () => (
+  <NesContainer title={<i className="nes-mario"></i>}>
+    <p>I am the hero</p>
+  </NesContainer>
+);
+
+export default Hero;
